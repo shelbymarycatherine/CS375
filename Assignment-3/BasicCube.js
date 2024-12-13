b17f896150d2245768b68fb6ee5d4046e00c1c9e
Shelby Anderson <shelbyanderson7030@gmail.com>
2024-12-12 21:06:21 -0800

    Update BasicCube.js

--- Assignment-3/BasicCube.js
+++ Assignment-3/BasicCube.js
@@ -1,17 +1,154 @@
-/////////////////////////////////////////////////////////////////////////////
-//
-//  BasicCube.js
-//
-//  A cube defined of 12 triangles
-//
+// BasicCube.js
+// A cube defined of 12 triangles
 
 class BasicCube {
     constructor(gl, vertexShader, fragmentShader) {
+        // Compile and link the shader program
+        this.program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+        // Define the cube's vertex positions (12 triangles = 36 vertices)
+        const vertices = new Float32Array([
+            // Front face
+            -0.5, -0.5,  0.5,
+             0.5, -0.5,  0.5,
+             0.5,  0.5,  0.5,
+            -0.5, -0.5,  0.5,
+             0.5,  0.5,  0.5,
+            -0.5,  0.5,  0.5,
 
-        this.draw = () => {
-            // program.use();
-        };
+            // Back face
+            -0.5, -0.5, -0.5,
+            -0.5,  0.5, -0.5,
+             0.5,  0.5, -0.5,
+            -0.5, -0.5, -0.5,
+             0.5,  0.5, -0.5,
+             0.5, -0.5, -0.5,
+
+            // Top face
+            -0.5,  0.5, -0.5,
+            -0.5,  0.5,  0.5,
+             0.5,  0.5,  0.5,
+            -0.5,  0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5,  0.5, -0.5,
+
+            // Bottom face
+            -0.5, -0.5, -0.5,
+             0.5, -0.5, -0.5,
+             0.5, -0.5,  0.5,
+            -0.5, -0.5, -0.5,
+             0.5, -0.5,  0.5,
+            -0.5, -0.5,  0.5,
+
+            // Right face
+             0.5, -0.5, -0.5,
+             0.5,  0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5, -0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5, -0.5,  0.5,
+
+            // Left face
+            -0.5, -0.5, -0.5,
+            -0.5, -0.5,  0.5,
+            -0.5,  0.5,  0.5,
+            -0.5, -0.5, -0.5,
+            -0.5,  0.5,  0.5,
+            -0.5,  0.5, -0.5
+        ]);
+
+        // Define colors for the cube (one color per vertex)
+        const colors = new Float32Array([
+            // Front face (red)
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            
+            // Back face (green)
+            0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 1.0, 0.0,
+
+            // Top face (blue)
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+
+            // Bottom face (yellow)
+            1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+
+            // Right face (cyan)
+            0.0, 1.0, 1.0,
+            0.0, 1.0, 1.0,
+            0.0, 1.0, 1.0,
+            0.0, 1.0, 1.0,
+            0.0, 1.0, 1.0,
+            0.0, 1.0, 1.0,
+
+            // Left face (magenta)
+            1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0
+        ]);
+
+        // Create buffers for vertex positions and colors
+        this.vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+        this.colorBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+        // Store attribute locations
+        this.aPosition = this.program.getAttributeLocation("aPosition");
+        this.aColor = this.program.getAttributeLocation("aColor");
+
+        // Initialize transformation matrices
+        this.MV = mat4();
+        this.P = mat4();
     }
-};
+
+    draw(gl) {
+        // Use the shader program
+        this.program.use();
+
+        // Set uniform matrices
+        this.program.setUniform("MV", this.MV);
+        this.program.setUniform("P", this.P);
+
+        // Bind and enable vertex buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.vertexAttribPointer(this.aPosition, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.aPosition);
+
+        // Bind and enable color buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+        gl.vertexAttribPointer(this.aColor, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.aColor);
+
+        // Draw the cube (36 vertices)
+        gl.drawArrays(gl.TRIANGLES, 0, 36);
+
+        // Disable attributes
+        gl.disableVertexAttribArray(this.aPosition);
+        gl.disableVertexAttribArray(this.aColor);
+    }
+}
